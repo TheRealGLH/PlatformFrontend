cab2da22e57ec20d1b7c565f3bb1d149982cc8e8
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2020-03-12 12:06:03 +0100

    adds ui methods to set player names in lobby

--- src/components/ScreenLobby.vue
+++ src/components/ScreenLobby.vue
@@ -4,9 +4,8 @@
       <div class="menu">
       <div class="menuBox">
 <h3>Players:</h3>
-<ul>
-    <li>LOREM</li>
-    <li>IPSUM</li>
+<ul >
+  <li v-for="player in players">{{ player.name }}</li>
 </ul>
 
 Select map:
@@ -21,11 +20,13 @@ Select map:
 <h3>Current map: {{ mapName }}</h3>
 <transition name="fade">
 <img id="lobbyMapPreview" :src="mapPreviewURL" @error="mapImageNotFound" :width="previewWidth" :height="previewHeight" :key="lobbyMapPreview"/>
-</transition>
+</transition><br/>
 <button>Start game</button>
 </div>
   </div>
   <button @click="testMapSwitch" disabled>Test mapswitch</button>
+  <button @click="testPlayerAdd">Test player</button>
+  <button @click="clearPlayers">clear players</button>
   </div>
 </template>
 
@@ -35,6 +36,8 @@ export default {
   data () {
     return {
       msg: 'In lobby',
+      players: [],
+      player: {name: '', number: 0},
       mapName: '%mapname%',
       mapPreviewDirectory: 'static/mappreview/',
       mapPreviewURL: 'static/mappreview/none.png',
@@ -42,7 +45,11 @@ export default {
       previewHeight: 225
     }
   },
+  mounted: function () {
+    this.setMapInfo('2fort')
+  },
   methods: {
+    // UI methods
     setMapInfo (mapName) {
       this.mapName = mapName
       this.mapPreviewURL = this.mapPreviewDirectory + this.mapName + '.png'
@@ -50,12 +57,29 @@ export default {
     mapImageNotFound (event) {
       event.target.src = this.mapPreviewDirectory + 'none.png'
     },
+    addPlayer (playerName, playerNumber) {
+      let newPlayer = {
+        name: playerName,
+        number: playerNumber
+      }
+      this.players.push(newPlayer)
+    },
+    clearPlayers () {
+      this.players = []
+    },
+    // Events
     onMapChange (event) {
       this.setMapInfo(event.target.value)
     },
+    // Test methods
     testMapSwitch () {
       var name = prompt('Give new name', 'battlefield')
       this.setMapInfo(name)
+    },
+    testPlayerAdd () {
+      var name = prompt('Give player name', 'Fred')
+      var number = prompt('Give player number', '12')
+      this.addPlayer(name, number)
     }
   }
 }
