e5266aeaabab3e02f8036b488009f7e03c4581a3
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2020-06-20 14:02:33 +0200

    game sprite tests

--- test/unit/specs/ScreenGame.spec.js
+++ test/unit/specs/ScreenGame.spec.js
@@ -5,8 +5,61 @@ describe('ScreenGame.vue', () => {
   it('should add sprites properly', () => {
     const wrapper = shallowMount(ScreenGame)
     const vm = wrapper.vm
-    //TODO
+    var id = 1;
+    var xPos = 1;
+    var yPos = 2;
+    var xScale = 1;
+    var yScale = 2;
+    vm.addSprite(id, 'PLAYER', xPos, yPos, xScale, yScale, false)
+    var sprite = vm.spriteMap.get(id)
+    expect(sprite).toBeDefined()
+    //Check position. Y pos is altered because Y0 is the bottom in game data, but the top of the screen.
+    expect(sprite.x).toEqual(xPos)
+    expect(sprite.y).toEqual(vm.h - yPos - vm.spriteSize * sprite.scaleY)
+    //Check scale, size is adjusted for screen size and size of the images
+    expect(sprite.scaleX).toEqual(xScale / vm.spriteSize / vm.spriteScaleFactor)
+    expect(sprite.scaleY).toEqual(yScale / vm.spriteSize / vm.spriteScaleFactor)
+  })
+  it('should update sprites properly', () => {
+    const wrapper = shallowMount(ScreenGame)
+    const vm = wrapper.vm
+    var anim = 'PLAYER'
+    var id = 1;
+    var xPos = 1;
+    var yPos = 2;
+    var xScale = 1;
+    var yScale = 2;
     
+    var posUpdateFactor = 3;
+    var scaleUpdateFactor = 4;
+    var animUpdate = 'PLATFORM'
+    vm.addSprite(id, 'PLAYER', xPos, yPos, xScale, yScale, false)
+    vm.updateSprite(id, animUpdate, xPos * posUpdateFactor, yPos * posUpdateFactor, xScale * scaleUpdateFactor, yScale * scaleUpdateFactor, true)
+    var sprite = vm.spriteMap.get(id)
+    expect(sprite).toBeDefined()
+    //Check position. X pos is altered because it's flipped. Y pos is altered because Y0 is the bottom in game data, but the top of the screen.
+    expect(sprite.x).toEqual(xPos * posUpdateFactor - sprite.scaleX * vm.spriteSize)
+    expect(sprite.y).toEqual(vm.h - yPos * posUpdateFactor - vm.spriteSize * sprite.scaleY)
+    //Check scale, size is adjusted for screen size and size of the images
+    expect(sprite.scaleX).toEqual(-xScale * scaleUpdateFactor / vm.spriteSize / vm.spriteScaleFactor)
+    expect(sprite.scaleY).toEqual(yScale * scaleUpdateFactor / vm.spriteSize / vm.spriteScaleFactor)
+  })
+  it('should update sprites properly', () => {
+    const wrapper = shallowMount(ScreenGame)
+    const vm = wrapper.vm
+    var anim = 'PLAYER'
+    var id = 1;
+    var xPos = 1;
+    var yPos = 2;
+    var xScale = 1;
+    var yScale = 2;
+    
+    vm.addSprite(id, 'PLAYER', xPos, yPos, xScale, yScale, false)
+    var sprite = vm.spriteMap.get(id)
+    expect(sprite).toBeDefined()
+    vm.deleteSprite(id)
+    sprite = vm.spriteMap.get(id)
+    expect(sprite).toBeUndefined()
   })
 
 })
